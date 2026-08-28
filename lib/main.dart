2e65b86062eb0f4c6f3516ca32294ee584eb64f6
matargaming <matargaming17@gmail.com>
2026-08-28 09:56:00 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'screens/saran_screen.dart';
 import 'screens/bonus_screen.dart';
 import 'screens/akun_screen.dart';
 import 'screens/home_screen.dart';
-import 'screens/login/login_gate.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,37 +14,8 @@ class DlovidApp extends StatelessWidget {
   Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginGate(),
+      home: MainNav(), // BYPASS LOGIN DULU
     );
   }
 }
-
-class MainNav extends StatefulWidget {
-  @override
-  _MainNavState createState() => _MainNavState();
-}
-
-class _MainNavState extends State<MainNav> {
-  int _index = 0;
-  final _pages = [SaranScreen(), HomeScreen(), BonusScreen(), AkunScreen()];
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: _pages[_index],
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.black,
-        selectedItemColor: Colors.amber,
-        unselectedItemColor: Colors.white54,
-        currentIndex: _index,
-        onTap: (i)=> setState(()=> _index=i),
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.recommend), label: "Saran"),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: "Bonus"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Akun"),
-        ],
-      ),
-    );
-  }
-}
+// ... MainNav tetep sama
