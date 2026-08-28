1bc65aed681e9b4128c2d1039da8c81ac4760e6a
matargaming <matargaming17@gmail.com>
2026-08-28 10:07:00 +0700

    Initialize Firebase and set up navigation
    Initialized Firebase in main function and updated home to LoginGate. Added MainNav class for navigation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'screens/saran_screen.dart';
 import 'screens/bonus_screen.dart';
 import 'screens/akun_screen.dart';
 import 'screens/home_screen.dart';
+import 'screens/login/login_gate.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(DlovidApp());
 }
 
@@ -14,8 +17,37 @@ class DlovidApp extends StatelessWidget {
   Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainNav(), // BYPASS LOGIN DULU
+      home: LoginGate(), // Balik ke LoginGate lagi, Firebase udah di-init!
     );
   }
 }
-// ... MainNav tetep sama
+
+class MainNav extends StatefulWidget {
+  @override
+  _MainNavState createState() => _MainNavState();
+}
+
+class _MainNavState extends State<MainNav> {
+  int _index = 0;
+  final _pages = [SaranScreen(), HomeScreen(), BonusScreen(), AkunScreen()];
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: _pages[_index],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.amber,
+        unselectedItemColor: Colors.white54,
+        currentIndex: _index,
+        onTap: (i)=> setState(()=> _index=i),
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.recommend), label: "Saran"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: "Bonus"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Akun"),
+        ],
+      ),
+    );
+  }
+}
