c6d1b9182e256ade9f8167d7f780aa368410556b
matargaming <matargaming17@gmail.com>
2026-08-30 02:29:10 +0700

    Update applicationId and namespace in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,17 +5,21 @@ plugins {
 }
 
 android {
-    namespace = "com.dlovid.shortvideo"
+    namespace = "com.dlovid.dlovids"
     compileSdk = 34
+
     defaultConfig {
-        applicationId = "com.dlovid.shortvideo"
+        applicationId = "com.dlovid.dlovids"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         manifestPlaceholders["ADMOB_APP_ID"] = System.getenv("ADMOB_APP_ID") ?: "ca-app-pub-3940256099942544~3347511713"
     }
-    buildTypes { release { isMinifyEnabled = false } }
+
+    buildTypes {
+        release { isMinifyEnabled = false }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -29,13 +33,8 @@ dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
     implementation(composeBom)
     implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.ui:ui-tooling")
-    implementation("androidx.compose.material:material-icons-extended")
-    implementation("io.coil-kt:coil-compose:2.5.0")
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-analytics-ktx")
