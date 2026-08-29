a5b21a50bef8b11842a6845c61af2b8574b851ac
matargaming <matargaming17@gmail.com>
2026-08-29 11:57:26 +0700

    Delete lib directory

--- lib/screens/admin/admin_login_2.dart
+++
@@ -1,2 +0,0 @@
-import 'package:flutter/material.dart';
-class AdminLogin2 extends StatelessWidget { const AdminLogin2({super.key}); @override Widget build(BuildContext context) { return const Scaffold(body: Center(child: Text("Admin Login 2"))); } }
