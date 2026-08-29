6fc15109c3310e1fa885b37f557b96bae0dc885b
matargaming <matargaming17@gmail.com>
2026-08-29 23:28:16 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,1 +1,10 @@
+// Top-level build file - IBUnya project
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+}
 
+task("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
