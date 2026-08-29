5c52379acb7a0de1e4a015efd17d6dd4306a3309
matargaming <matargaming17@gmail.com>
2026-08-29 09:52:42 +0700

    Delete lib directory

--- lib/screens/admin/admin_gate_screen.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-class AdminGateScreen extends StatelessWidget{
-  const AdminGateScreen({super.key});
-  @override
-  Widget build(BuildContext context)=> const Scaffold(body: Center(child: Text('Admin Gate')));
-}
