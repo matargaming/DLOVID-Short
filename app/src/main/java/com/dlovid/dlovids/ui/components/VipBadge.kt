19c3d315c76f632ba531a1af2819221416c1cd38
matargaming <matargaming17@gmail.com>
2026-08-30 10:30:25 +0700

    Update VipBadge.kt

--- app/src/main/java/com/dlovid/dlovids/ui/components/VipBadge.kt
+++ app/src/main/java/com/dlovid/dlovids/ui/components/VipBadge.kt
@@ -1,6 +1,8 @@
-package com.dlov.gold.ui.components
+package com.dlovid.dlovids.ui.components
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+
 @Composable
 fun VipBadge(isVip: Boolean) {
-    if(isVip) Text("VIP 👑") else Text("Beli VIP")
+    if(isVip) Text("VIP AKTIF 👑") else Text("BELI VIP 1 BULAN 30.000")
 }
