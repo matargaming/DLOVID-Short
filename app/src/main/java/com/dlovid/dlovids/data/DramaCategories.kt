e001316f281ca51d2103abd823c7ada6e640abcd
matargaming <matargaming17@gmail.com>
2026-08-30 18:31:44 +0700

    Add DramaCategories object with predefined categories

---
+++ app/src/main/java/com/dlovid/dlovids/data/DramaCategories.kt
@@ -0,0 +1,9 @@
+package com.dlovid.dlovids.data
+
+object DramaCategories {
+    val list = listOf(
+        "Terpopuler", "Romantis", "Sedih", "Balas Dendam",
+        "Keluarga", "Horor", "Komedi", "Action",
+        "Drama China", "Drama Korea", "Indo"
+    )
+}
