65356de90ff4f8611de12b4c323e5ddfcbb216d1
matargaming <matargaming17@gmail.com>
2026-08-30 21:11:53 +0700

    Delete app/src/main/java/com/dlovid/dlovids/ui directory

--- app/src/main/java/com/dlovid/dlovids/ui/components/VipBadge.kt
+++
@@ -1,8 +0,0 @@
-package com.dlovid.dlovids.ui.components
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun VipBadge(isVip: Boolean) {
-    if(isVip) Text("VIP AKTIF 👑") else Text("BELI VIP 1 BULAN 30.000")
-}
