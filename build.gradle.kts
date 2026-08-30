8a3a158af79dc57cddabcbaf156933fb4307deb8
matargaming <matargaming17@gmail.com>
2026-08-30 11:16:37 +0700

    Refactor clean task to use layout for build directory

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,6 @@ plugins {
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
-task("clean", Delete::class) {
-    delete(rootProject.buildDir)
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
