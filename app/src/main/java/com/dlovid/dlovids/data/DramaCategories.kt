b5ec3c7ad1a5f596870e96030008c55818ce8881
matargaming <matargaming17@gmail.com>
2026-08-30 21:11:07 +0700

    Delete app/src/main/java/com/dlovid/dlovids/data/DramaCategories.kt

--- app/src/main/java/com/dlovid/dlovids/data/DramaCategories.kt
+++
@@ -1,9 +0,0 @@
-package com.dlovid.dlovids.data
-
-object DramaCategories {
-    val list = listOf(
-        "Terpopuler", "Romantis", "Sedih", "Balas Dendam",
-        "Keluarga", "Horor", "Komedi", "Action",
-        "Drama China", "Drama Korea", "Indo"
-    )
-}
