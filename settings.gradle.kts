64333835efcd48be0d0b281a6c7094fbf5bea81d
matargaming <matargaming17@gmail.com>
2026-08-30 11:14:57 +0700

    Add JitPack repository to plugin and dependency management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,17 @@
 pluginManagement {
-    repositories { google(); mavenCentral(); gradlePluginPortal() }
+    repositories { 
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
+    }
 }
 dependencyResolutionManagement {
-    repositories { google(); mavenCentral() }
+    repositories { 
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
 }
 rootProject.name = "DLOVID-Short"
 include(":app")
