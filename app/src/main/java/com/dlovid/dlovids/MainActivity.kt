d6102f5d92e1a5b89a3ea0128140586637aa35f8
matargaming <matargaming17@gmail.com>
2026-09-03 20:43:33 +0700

    Update MainActivity.kt

--- app/src/main/java/com/dlovid/dlovids/MainActivity.kt
+++ app/src/main/java/com/dlovid/dlovids/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.dlovid.dlovids
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var viewPager: ViewPager2
+
+    // 3 Video sample buat tes FYP
+    private val sampleVideos = listOf(
+        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         viewPager = findViewById(R.id.viewPagerVideo)
         viewPager.orientation = ViewPager2.ORIENTATION_VERTICAL
+
+        // Pasang adapter
+        val adapter = VideoAdapter(sampleVideos)
+        viewPager.adapter = adapter
     }
 }
